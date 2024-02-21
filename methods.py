8e1cb949797b8a77fa3012f7dc2801ce12da1965
RawanHatem28 <rawanhatem8228@gmail.com>
2024-02-21 21:35:12 +0200

    implemented search and update methods

--- methods.py
+++ methods.py
@@ -35,36 +35,32 @@ def viewStudent():
 def searchStudent():
     global students
     student_id = 0
-
     if not students:
         print("There are no students to search for.")
         return
 
     try:
         search_by = input("Would you like to Search for a student by Name or ID: ")
-        if search_by.lower() == 'name':
-            student_name = input("Enter Name of student to search for: ")
-        elif search_by.lower() == 'id':
-            student_id = int(input("Enter ID of student to search for: "))
+        if search_by.lower() not in ['name', 'id']:
+            print("Invalid input! ")
     except ValueError:
         print("Invalid input. Please enter a valid integer for the student ID.")
         return
 
-
+    search_value = input(f"Enter {search_by}: ")
     found = False
 
     for student in students:
-        if student['ID'] == student_id or student['Name'] == student_name:
+        if  student['ID'] == int(search_value) or student['Name'] == search_value:
             print(f"The student was found Successfully ")
             print("\t\t\" The Student Info\"" )
-            print("\n\t\t==================")
+            print("\t\t==================")
             for key, value in student.items():
                 print(f"{key}: {value}")
                 found = True
             break
-        elif student['ID'] != student_id or student['Name'] != student_name:
-            print("There is No Student With This Information Please CHECK And Write Correct Information")
-
+        # elif student['ID'] != student_id or student['Name'] != student_name:
+        #     print("There is No Student With This Information Please CHECK And Write Correct Information")
 
     if not found:
         print(f" SORRY! Student was NOT found")
@@ -72,7 +68,6 @@ def searchStudent():
 
 def updateStudentDetails():
     global students
-    global student_id
 
     if not students:
         print("There are no students to update.")
@@ -84,44 +79,34 @@ def updateStudentDetails():
         print("Invalid input. Please enter a valid integer for the student ID.")
         return
 
-    updated = False
-
     for index, student in enumerate(students):
         if student['ID'] == student_id:
             print(f"The student who has this ID number: {student_id} was found Successfully ")
             update_type = int(input("Enter your command (choose number 1 or 2 ) : "
                                     "\n\t 1. Update All Student Details"
-                                    "\n\t 2. Update One Value Of Student Details "))
+                                    "\n\t 2. Update One Value Of Student Details \n"))
             if update_type == 1:
                 name = error_handling.handleString(input("Full name: "))
                 age = error_handling.handleSignal(input("Age: "))
                 grade = error_handling.handleSignal(input("Grade: "))
                 email = error_handling.handleEmail(input("Email: "))
                 address = error_handling.handleString(input("Address: "))
-                student = {
-                    "Name": name,
-                    "Age": age,
-                    "Grade": grade,
-                    "Email": email,
-                    "Address": address
-                }
-                students.insert(index, student)
-                print("\n\t\"The New Student Details Has Successfully Updated To\"")
-                for student in students:
-                    print("=============================================================")
-                    for key, value in student.items():
-                        print(f"{key}: {value}")
-                print("=============================================================")
-                break
 
+                students[index]['Name'] = name
+                students[index]['Age'] = age
+                students[index]['Grade'] = grade
+                students[index]['Email'] = email
+                students[index]['Address'] = address
+
+                print("\n\t\"The New Student Details Has Successfully Updated To\"")
 
             elif update_type == 2:
-                update_by_value = input("Enter your command (choose number of update value you want to update): "
-                                        "\n\t 1.Name"
-                                        "\n\t 2.Age"
-                                        "\n\t 3.Grade"
-                                        "\n\t 4.Email"
-                                        "\n\t 5.Address")
+                update_by_value = int(input("Enter your command (choose number of update value you want to update): "
+                                            "\n\t 1.Name"
+                                            "\n\t 2.Age"
+                                            "\n\t 3.Grade"
+                                            "\n\t 4.Email"
+                                            "\n\t 5.Address\n"))
 
                 if update_by_value == 1:
                     name = error_handling.handleString(input("Full name: "))
@@ -140,6 +125,14 @@ def updateStudentDetails():
                     student['Address'] = address
                 else:
                     print("Invalid command, Please Enter Number from 1 to 5.")
+                update_value ={
+                    1: 'Name',
+                    2: 'Age',
+                    3: 'Grade',
+                    4: 'Email',
+                    5: 'Address'
+                }
+                print(f"\t\"The {update_value[update_by_value]} Has Updated Successfully To\"")
 
 
 def deleteStudentById():
