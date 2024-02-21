2aecd49d72f904c915bd2e324f51a69d9a062d27
Salma Swailem <100314367+Salma-Swailem@users.noreply.github.com>
2024-02-22 03:29:51 +0400

    Updated

--- methods.py
+++ methods.py
@@ -27,13 +27,119 @@ def addStudent():
 
 def viewStudent():
     """ method prints all the students in the temp database """
+    if len(students) == 0:
+        print("There is no studen to view.")
+        return
     for student in students:
         print("=============================================================")
         for key, value in student.items():
             print(f"{key}: {value}")
     print("=============================================================")
-    
-    
+
+
+def searchStudent():
+    """ method search for a student by name or id in the temp database"""
+    global students
+
+    if not students:
+        print("There are no students to search for.")
+        return
+
+    try:
+        search_by = input("Would you like to Search for a student by Name or ID: ")
+        if search_by.lower() not in ['name', 'id']:
+            print("Invalid input! ")
+    except ValueError:
+        print("Invalid input. Please enter a valid integer for the student ID.")
+        return
+
+    search_value = input(f"Enter {search_by}: ").strip()
+    found = False
+
+    for student in students:
+        if student['ID'] == int(search_value) or student['Name'] == search_value:
+            print(f"The student was found Successfully ")
+            print("\t\t\" The Student Info\"" )
+            print("\t\t==================")
+            for key, value in student.items():
+                print(f"{key}: {value}")
+                found = True
+            break
+
+    if not found:
+        print(f" SORRY! Student was NOT found")
+
+
+def updateStudentDetails():
+    """ update student details method by ID , it can update all student details or One student detail """
+    global students
+
+    if not students:
+        print("There are no students to update.")
+        return
+
+    try:
+        student_id = int(input("Enter ID of student to update: "))
+    except ValueError:
+        print("Invalid input. Please enter a valid integer for the student ID.")
+        return
+
+    for index, student in enumerate(students):
+        if student['ID'] == student_id:
+            print(f"The student who has this ID number: {student_id} was found Successfully ")
+            update_type = int(input("Enter your command (choose number 1 or 2 ) : "
+                                    "\n\t 1. Update All Student Details"
+                                    "\n\t 2. Update One Value Of Student Details \n"))
+            if update_type == 1:
+                name = error_handling.handleString(input("Full name: "))
+                age = error_handling.handleSignal(input("Age: "))
+                grade = error_handling.handleSignal(input("Grade: "))
+                email = error_handling.handleEmail(input("Email: "))
+                address = error_handling.handleString(input("Address: "))
+
+                students[index]['Name'] = name
+                students[index]['Age'] = age
+                students[index]['Grade'] = grade
+                students[index]['Email'] = email
+                students[index]['Address'] = address
+
+                print("\n\t\"The New Student Details Has Successfully Updated To\"")
+
+            elif update_type == 2:
+                update_by_value = int(input("Enter your command (choose number of update value you want to update): "
+                                            "\n\t 1.Name"
+                                            "\n\t 2.Age"
+                                            "\n\t 3.Grade"
+                                            "\n\t 4.Email"
+                                            "\n\t 5.Address\n"))
+
+                if update_by_value == 1:
+                    name = error_handling.handleString(input("Full name: "))
+                    student['Name'] = name
+                elif update_by_value == 2:
+                    age = error_handling.handleSignal(input("Age: "))
+                    student['Age'] = age
+                elif update_by_value == 3:
+                    grade = error_handling.handleSignal(input("Grade: "))
+                    student['Grade'] = grade
+                elif update_by_value == 4:
+                    email = error_handling.handleEmail(input("Email: "))
+                    student['Email'] = email
+                elif update_by_value == 5:
+                    address = error_handling.handleString(input("Address: "))
+                    student['Address'] = address
+                else:
+                    print("Invalid command, Please Enter Number from 1 to 5.")
+                update_value ={
+                    1: 'Name',
+                    2: 'Age',
+                    3: 'Grade',
+                    4: 'Email',
+                    5: 'Address'
+                }
+                print(f"\t\"The {update_value[update_by_value]} Has Updated Successfully To\"")
+
+
 def deleteStudentById():
     """
     Delete a student from the list of students by ID.
@@ -49,7 +155,6 @@ def deleteStudentById():
     Returns:
     None
     """
-
     global students
 
     if not students:
@@ -76,118 +181,23 @@ def deleteStudentById():
         print("Student not found")
 
 
-
-def searchStudent():
-    
-    student_id=0
-    
-    if not students:
-        print("There are no students to search for.")
-
-    try:
-        search_by = input("Would you like to Search for a student by Name or ID: ")
-        if search_by.lower() == 'name':
-            student_name = input("Enter Name of student to search for: ")
-        elif search_by.lower() == 'id':
-            student_id = int(input("Enter ID of student to search for: "))
-    except ValueError:
-        print("Invalid input. Please enter a valid integer for the student ID.")
-        return
-
-
-    found = False
-
-    for student in students:
-        if student['ID'] == student_id or student['Name'] == student_name:
-            print(f"The student was found Successfully ")
-            for key, value in student.items():
-                print(f"{key}: {value}")
-                found = True
-            break
-
-    if not found:
-        print(f" SORRY! Student was NOT found")
-
-
-def updateStudentDetails():
-
-    if not students:
-        print("There are no students to update.")
-        return
-
-    try:
-        student_id = int(input("Enter ID of student to update: "))
-    except ValueError:
-        print("Invalid input. Please enter a valid integer for the student ID.")
-        return
-
-    updated = False
-
-    for index, student in enumerate(students):
-        if student['ID'] == student_id:
-            print(f"The student who has this ID number: {student_id} was found Successfully ")
-            update_type = int(input("Enter your command (choose number 1 or 2 ) : "
-                                    "\n\t 1. Update All Student Details"
-                                    "\n\t 2. Update One Value Of Student Details "))
-            if update_type == 1:
-                name = error_handling.handleString(input("Full name: "))
-                age = error_handling.handleSignal(input("Age: "))
-                grade = error_handling.handleSignal(input("Grade: "))
-                email = error_handling.handleEmail(input("Email: "))
-                address = error_handling.handleString(input("Address: "))
-                student = {
-                    "Name": name,
-                    "Age": age,
-                    "Grade": grade,
-                    "Email": email,
-                    "Address": address
-                }
-                students.insert(index, student)
-            elif update_type == 2:
-                update_by_value = input("Enter your command (choose number of update value you want to update): "
-                                        "\n\t 1.Name"
-                                        "\n\t 2.Age"
-                                        "\n\t 3.Grade"
-                                        "\n\t 4.Email"
-                                        "\n\t 5.Address")
-
-                if update_by_value == 1:
-                    name = error_handling.handleString(input("Full name: "))
-                    student['Name'] = name
-                elif update_by_value == 2:
-                    age = error_handling.handleSignal(input("Age: "))
-                    student['Age'] = age
-                elif update_by_value == 3:
-                    grade = error_handling.handleSignal(input("Grade: "))
-                    student['Grade'] = grade
-                elif update_by_value == 4:
-                    email = error_handling.handleEmail(input("Email: "))
-                    student['Email'] = email
-                elif update_by_value == 5:
-                    address = error_handling.handleString(input("Address: "))
-                    student['Address'] = address
-                else:
-                    print("Invalid command, Enter numbers 1 to 5.")
-
-
-
 def loadFromFile():
-    file_list=''   # to store content of file 
+    file_list=''   # to store content of file
     with open("database.txt","r") as file:
         file_list=f"[{file.read()}]"
         if file_list=='[]':
             print("file is empty :(")
         else:
             file_list=eval(file_list) # to convert str to tuple or list
-            
+
             for i in file_list:   # to rem a dublicated value in list
                 if i not in students:
                     students.append(i)
                 else:
-                    continue  
+                    continue
 
 def saveToFile():
-    
+
     with open("database.txt","a") as file:
         with open("database.txt","r") as filee_test:
             test=filee_test.read()
